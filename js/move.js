823a0b8905d99a3f1900b6814c0b86a443938b83
CeliaChou <celiachou0413@gmail.com>
2022-03-02 17:08:26 +0800

    test

---
+++ js/move.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $(".masterBtn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part1").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".winterBtn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part2").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".caneBtn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part3").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".xmasBtn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part4").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".nav1").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part1").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".nav2").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part2").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".nav3").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part3").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".nav4").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#part4").offset().top
+        }, 500);
+    });
+});
+$(document).ready(function() {
+    $(".nav6").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".part0").offset().top
+        }, 500);
+    });
+});
